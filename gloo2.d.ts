7b32dfd409a9adaa29628a29dc82ddcae77bcf8a
Mateusz Paprocki <mattpap@gmail.com>
2018-01-18 11:10:52 +0100

    Add type declarations

---
+++ gloo2.d.ts
@@ -0,0 +1,55 @@
+/*
+type Mode = "POINTS" | "LINES" | "LINE_LOOP" | "LINE_STRIP" | "LINE_FAN" | "TRIANGLES"
+
+type Wrap = "REPEAT" | "CLAMP_TO_EDGE" | "MIRRORED_REPEAT"
+
+type MinInterp = "LINEAR" | "NEAREST"
+
+type MaxInterp = "LINEAR" | "NEAREST" | "NEAREST_MIPMAP_NEAREST" | "LINEAR_MIPMAP_NEAREST" | "NEAREST_MIPMAP_LINEAR" | "LINEAR_MIPMAP_LINEAR"
+
+type TextureFormat = "LUMINANCE" | "LUMINANCE_ALPHA" | "RGB" | "RGBA"
+*/
+
+export declare type GLEnum = number
+
+export declare class GlooObject {
+  constructor(gl: WebGLRenderingContext)
+}
+
+export declare class Program extends GlooObject {
+  delete(): void
+  activate(): void
+  deactivate(): void
+  set_shaders(vert: string, frag: string): void
+  set_texture(name: string, value: Texture2D): void
+  set_uniform(name: string, type_: string, value: number[]): void
+  set_attribute(name: string, type_: string, value: VertexBuffer | number[], stride?: number, offset?: number): void
+  draw(mode: GLEnum /*Mode*/, selection: IndexBuffer | [number, number]): void
+}
+
+export declare class Buffer extends GlooObject {
+  delete(): void
+  activate(): void
+  deactivate(): void
+  set_size(nbytes: number): void
+  set_data(offset: number, data: any /* XXX: a typed array */): void
+}
+
+export declare class VertexBuffer extends Buffer {}
+
+export declare class IndexBuffer extends Buffer {}
+
+export declare class Texture2D extends GlooObject {
+  delete(): void
+  activate(): void
+  deactivate(): void
+  set_wrapping(wrap_s: GLEnum /*Wrap*/, wrap_t: GLEnum /*Wrap*/): void
+  set_interpolation(min: GLEnum /*MinInterp*/, max: GLEnum /*MaxInterp*/): void
+  set_size(shape: [number, number], format: GLEnum /*TextureFormat*/): void
+  set_data(offset: [number, number], shape: [number, number], data: any /* XXX: byte typed array */): void
+}
+
+export declare class Texture3DLike extends Texture2D {
+  set_size(shape: [number, number, number], format: GLEnum /*TextureFormat*/): void
+  set_data(offset: [number, number, number], shape: [number, number, number], data: any /* XXX: byte typed array */): void
+}
